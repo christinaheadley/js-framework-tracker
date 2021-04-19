d601116fd49e178c17410ca7a5f55283ecfb5249
christina <chrstnaheadley@gmail.com>
2021-04-19 14:28:15 -0400

    initial

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import App from "./App.vue";
+import HighchartsVue from "highcharts-vue";
+// import axios from "axios";
+// import Highcharts from "highcharts";
+
+Vue.config.productionTip = false;
+Vue.use(HighchartsVue);
+
+new Vue({
+  el: "#app",
+  render: (h) => h(App),
+}).$mount("#app");
